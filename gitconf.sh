00035771f3ada3d9326b787ae41748aea5771562
Thariq Aulia Akbar <thariq2712@gmail.com>
2020-07-29 11:39:24 +0700

    modify something

--- gitconf.sh
+++ gitconf.sh
@@ -4,22 +4,9 @@
 # Github : github.com/Qiraht
 #
 
-
-
-
-# check if apt-get is installed
-if which apt-get > /dev/null; then
-		sudo apt-get install -y git
-fi
-
-# check if pacman is installed
-if which pacman > /dev/null; then
-	sudo pacman -S --noconfirm git
-fi
-
+sudo apt install git -y
 
 # Setting up git
-
 git init
 git config --global user.name "Thariq Aulia Akbar"
 git config --global user.email "thariq2712@gmail.com"
