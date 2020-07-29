f3c7c81c6f82d058a741df1fc1f14d2e008ec948
Thariq Aulia Akbar <thariq2712@gmail.com>
2020-07-29 14:46:05 +0700

    edited some

--- gitconf.sh
+++ gitconf.sh
@@ -7,6 +7,8 @@
 sudo apt install git -y
 
 # Setting up git
-git init
 git config --global user.name "Thariq Aulia Akbar"
 git config --global user.email "thariq2712@gmail.com"
+
+clear
+echo "Git username and email hasbeen set"
