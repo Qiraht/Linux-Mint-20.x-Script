f77afefe081f79c64eb646bb46e1a6a9575cb1b0
Thariq Aulia Akbar <thariq2712@gmail.com>
2020-07-30 19:33:44 +0700

    Add new feature

--- gitconf.sh
+++ gitconf.sh
@@ -7,9 +7,14 @@
 sudo apt install git -y
 
 # Setting up git
-# dont forget to chenge to your own
-git config --global user.name "Your Username"
-git config --global user.email "your@email.com"
+clear
+echo -n "Setting up your git"
+echo -n "Insert your username  "
+read name
+git config --global user.name "$name"
+echo -n "Insert your email "
+read email
+git config --global user.email "$email"
 
 clear
 echo "Git username and email hasbeen set"
